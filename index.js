9ed4abc89d619feb96ec8043b20609c60b4421d8
sajidhussaingondal <sajidgondal@gmail.com>
2023-01-12 17:47:31 +0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+Create An Function Which Can Take Any Number 
+Of Parameters And Do Addition Of All And Then Return 
+The Result To Result Variable And After That Show The 
+Result Using  Console
+*/
+
+function doCalculations (){
+    
+}
