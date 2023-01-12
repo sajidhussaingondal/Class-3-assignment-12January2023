f7fc6d324205959c8a8eb521e54be7c54f7b4dc3
sajidhussaingondal <sajidgondal@gmail.com>
2023-01-12 19:07:18 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function doCalculations(...numbers) {
   return doCalculations;
 }
 
-let doAdditionOf = doCalculations(1, 2, 3, 4, 5, 6, 7, 8);
+let doAdditionOf = doCalculations(1, 2, 3);
 console.log(doAdditionOf);
 // doCalculations(1, 2, 3, 4, 5, 6, 7, 8);
 // console.log(doCalculations);
