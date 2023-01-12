2c4baf6c67faf97f0dd74b224cbc6a040fcaf6ac
sajidhussaingondal <sajidgondal@gmail.com>
2023-01-12 18:14:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ The Result To Result Variable And After That Show The
 Result Using  Console
 */
 
-function doCalculations (...numbers){
-    let sum = 0;
-    for (let arg of numbers) sum += arg;
-    return sum;
-  }
-  
-  let doAdditionOf = doCalculations(1, 2, 3, 4, 5, 6, 7, 8);
+function doCalculations(...numbers) {
+  // function is defined, name of function is doCalculations. Rest parameters have been given using three dots (...) and
+  // parameters name (numbers)
+  let doCalculations = 0; //
+  for (let arg of numbers) doCalculations += arg;
+  return doCalculations;
+}
+
+let doAdditionOf = doCalculations(1, 2, 3, 4, 5, 6, 7, 8);
 console.log(doAdditionOf);
+// doCalculations(1, 2, 3, 4, 5, 6, 7, 8);
+// console.log(doCalculations);
