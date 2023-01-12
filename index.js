8445a2bafbc5db6ee612af1e4b9b05f7076ac5eb
sajidhussaingondal <sajidgondal@gmail.com>
2023-01-12 17:59:50 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ The Result To Result Variable And After That Show The
 Result Using  Console
 */
 
-function doCalculations (){
-    
-}
+function doCalculations (...numbers){
+    let sum = 0;
+    for (let arg of numbers) sum += arg;
+    return sum;
+  }
+  
+  let x = doCalculations(1, 2, 3, 4, 5, 6, 7, 8);
+console.log(x);
